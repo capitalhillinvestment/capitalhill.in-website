737f8e34704daa7d4398d376c01d1d70a4851072
capitalhillinvestment <capitalhillinvestment77@gmail.com>
2026-06-06 11:32:56 +0530

    Fix logo import path in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Menu, X, TrendingUp, ChevronDown } from 'lucide-react';
-import logo from '../assets/logo.png';
+import logo from '../assets/log.png';
 
 type Page = string;
 
