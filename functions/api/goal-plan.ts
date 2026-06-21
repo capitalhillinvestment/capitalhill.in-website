bd8a5485de57407d1fb17735bd795c2e2f6f54da
capitalhillinvestment <capitalhillinvestment77@gmail.com>
2026-06-21 18:50:30 +0530

    Add GET route for Goal Plan API

--- functions/api/goal-plan.ts
+++ functions/api/goal-plan.ts
@@ -9,3 +9,17 @@ export const onRequestPost = async () => {
     }
   );
 };
+
+export const onRequestGet = async () => {
+  return new Response(
+    JSON.stringify({
+      success: true,
+      message: "Goal Plan GET route works"
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+};
