42aa35c19f31b670717a4fd274f6405be79be974
capitalhillinvestment <capitalhillinvestment77@gmail.com>
2026-06-21 16:07:10 +0530

    Update fundRisk.ts

--- functions/data/funds/fundRisk.ts
+++ functions/data/funds/fundRisk.ts
@@ -1,1 +1,20 @@
 
+import { mutualFunds } from "../mutualFunds";
+
+export const fundRisk = mutualFunds.map((fund) => ({
+  id: fund.id,
+  name: fund.name,
+
+  // risk profile
+  riskLevel: fund.riskLevel,
+
+  // cost factor
+  expenseRatio: fund.expenseRatio,
+
+  // quality score
+  rating: fund.rating,
+
+  // optional safety indicator
+  category: fund.category,
+  subCategory: fund.subCategory,
+}));
