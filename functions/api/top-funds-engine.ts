4f6b5a95348902de8b8cfc038a7fb7c862279351
capitalhillinvestment <capitalhillinvestment77@gmail.com>
2026-06-23 23:30:31 +0530

    Implement top funds engine to calculate scores

---
+++ functions/api/top-funds-engine.ts
@@ -0,0 +1,19 @@
+import { mutualFunds } from "../data/mutualFunds";
+import { calculateFundScore } from "../../src/engine/calculateFundScore";
+
+export async function onRequestGet() {
+  const scored = mutualFunds.map((fund) => ({
+    ...fund,
+    score: calculateFundScore(fund),
+  }));
+
+  const topFunds = scored
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 10);
+
+  return Response.json({
+    success: true,
+    count: topFunds.length,
+    data: topFunds,
+  });
+}
