302337fc5331f9bde3cd13a8b2e3613661983e3d
capitalhillinvestment <capitalhillinvestment77@gmail.com>
2026-07-06 23:46:56 +0530

    Implement syncAMFI function to process AMFI data

--- functions/services/amfi/sync.ts
+++ functions/services/amfi/sync.ts
@@ -1,1 +1,17 @@
+import { downloadAMFI } from "./downloader";
+import { parseAMFI } from "./parser";
+import { validateFunds } from "./validator";
+import { normalizeFunds } from "./normalizer";
+import type { AMFIFund } from "./types";
 
+export async function syncAMFI(): Promise<AMFIFund[]> {
+  const rawData = await downloadAMFI();
+
+  const parsedFunds = parseAMFI(rawData);
+
+  const validFunds = validateFunds(parsedFunds);
+
+  const normalizedFunds = normalizeFunds(validFunds);
+
+  return normalizedFunds;
+}
