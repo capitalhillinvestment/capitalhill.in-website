265094dfc7321cb6a8360a989de7386e614959b0
capitalhillinvestment <capitalhillinvestment77@gmail.com>
2026-07-06 23:45:07 +0530

    Add validateFunds function to filter valid AMFI funds

--- functions/services/amfi/validator.ts
+++ functions/services/amfi/validator.ts
@@ -1,1 +1,13 @@
+import { AMFIFund } from "./types";
+
+export function validateFunds(funds: AMFIFund[]): AMFIFund[] {
+  return funds.filter(
+    (fund) =>
+      fund.schemeCode &&
+      fund.schemeName &&
+      !isNaN(fund.nav) &&
+      fund.nav > 0 &&
+      fund.date
+  );
+}
 
